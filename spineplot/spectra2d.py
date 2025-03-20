d3385585fab714a35c35c6bcb757aa81d104826d
Justin Mueller <justin.mueller@colostate.edu>
2025-03-20 08:41:10 -0500

    Implement fix for scientific limits on y-axis when log scale is set

--- spineplot/spectra2d.py
+++ spineplot/spectra2d.py
@@ -300,12 +300,12 @@ class SpineSpectra2D(SpineSpectra):
                     l.append('MC Statistical Uncertainty')
                 ax.legend(h, l)
         
-        if style.scilimits:
+        if style.scilimits and not logy:
             ax.ticklabel_format(axis='y', scilimits=style.scilimits)
         if style.mark_pot:
             mark_pot(ax, self._exposure, style.mark_pot_horizontal)
         if style.mark_preliminary is not None:
-            mark_preliminary(ax, style.mark_preliminary, hadj=0.035 if style.scilimits is not None else 0)
+            mark_preliminary(ax, style.mark_preliminary, hadj=0.035 if (style.scilimits and not logy) is not None else 0)
 
         # Set the axis to be logarithmic if requested.
         if logx:
